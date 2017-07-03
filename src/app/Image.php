588c6d3b22a142170732d438027137b62619dc66
jibo <jibo@outlook.com>
2017-07-03 17:28:14 +0800

    Update image view

--- src/app/Image.php
+++ src/app/Image.php
@@ -9,7 +9,20 @@ class Image
     function get($f3)
     {
         $image = ImageHelper::get($f3);
-        header('Content-Type:' . \Web::instance()->mime($image));
-        echo file_get_contents($image);
+        $context = substr($image, strlen(ImageHelper::$TARGET));
+        $name = preg_replace('/\//', '_', $context);
+
+        $dir = WEB . '/download';
+        if (!is_dir($dir)) {
+            mkdir($dir);
+        }
+
+        $file = $dir . '/' . $name;
+        if (!is_file($file)) {
+            file_put_contents($file, file_get_contents($image));
+        }
+
+        $f3->set('image', $f3->get('BASE') . '/download/' . $name);
+        echo \Template::instance()->render('image.html');
     }
 }
--- src/helper/Image.php
+++ src/helper/Image.php
@@ -7,16 +7,17 @@ use Symfony\Component\DomCrawler\Crawler;
 
 class Image
 {
+    public static $TARGET = 'http://www.mmjpg.com';
+
     public static function get($f3)
     {
-        $target = 'http://www.mmjpg.com';
         $crawler = new Crawler();
 
         // 解析相册总数
         $albumCount = $f3->get('ALBUM_COUNT');
         if (empty($albumCount)) {
             $crawler->clear();
-            $response = Request::get($target)->send();
+            $response = Request::get(self::$TARGET)->send();
             $crawler->addHtmlContent($response->body);
             $latest = $crawler->filter('.main .pic ul li>a')->eq(0)->attr('href');
             $path = explode('/', $latest);
@@ -28,7 +29,7 @@ class Image
         // 随机选择相册解析图片总数
         $crawler->clear();
         $album = random_int(1, $albumCount);
-        $response = Request::get($target . '/mm/' . $album)->send();
+        $response = Request::get(self::$TARGET . '/mm/' . $album)->send();
         $crawler->addHtmlContent($response->body);
         $imageNav = $crawler->filter('#page a');
         $lastImageHref = $imageNav->eq($imageNav->count() - 2)->attr('href');
@@ -37,18 +38,18 @@ class Image
         $path[count($path) - 1] = random_int(1, $imageCount);
         $f3->log('Album {album} with {imageCount} images',
             [
-                'album' => $target . '/mm/' . $album,
+                'album' => self::$TARGET . '/mm/' . $album,
                 'imageCount' => $imageCount
             ]
         );
 
         // 随机选择图片
         $crawler->clear();
-        $response = Request::get($target . implode('/', $path))->send();
+        $response = Request::get(self::$TARGET . implode('/', $path))->send();
         $crawler->addHtmlContent($response->body);
         $image = $crawler->filter('#content img')->eq(0)->attr('src');
         array_pop($path);
-        $f3->set('LAST_VISIT_ALBUM', $target . implode('/', $path), 600);
+        $f3->set('LAST_VISIT_ALBUM', self::$TARGET . implode('/', $path), 600);
         return $image;
     }
 }
