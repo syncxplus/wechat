7891dbdd73ed82384546c5ce35be3e2a847fb634
jibo <jibo@outlook.com>
2017-07-03 14:36:56 +0800

    Update message handler

--- src/app/Index.php
+++ src/app/Index.php
@@ -3,8 +3,8 @@
 namespace app;
 
 use EasyWeChat\Foundation\Application;
+use EasyWeChat\Message\Image;
 use EasyWeChat\Message\Text;
-use helper\Image;
 use helper\Option;
 
 class Index
@@ -15,13 +15,20 @@ class Index
         $app->server->setMessageHandler(function ($message) use ($app, $f3) {
             switch ($message->MsgType) {
                 case 'text':
-                    if ($message->Content == '1') {
+                    if ($message->Content == 'album') {
                         $lastVisitAlbum = $f3->get('LAST_VISIT_ALBUM');
                         if (!empty($lastVisitAlbum)) {
                             return new Text(['content' => $lastVisitAlbum]);
                         }
                     }
-                    return new Text(['content' => $f3->get('WECHAT_SERVER') . '/image']);
+                    return new Text(['content' => $message->Content]);
+                case 'image':
+                    file_put_contents('/tmp/images.log', $message->PicUrl, LOCK_EX | FILE_APPEND);
+                    return new Image(['media_id' => $message->MediaId]);
+                default:
+                    ob_start();
+                    var_dump($message);
+                    $f3->log(ob_get_clean());
             }
             return new Text(['content' => 'Hello world']);
         });
