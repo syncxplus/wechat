ca0cc12b4e7da891b7edbc6f156240562727e225
jibo <jibo@outlook.com>
2017-07-23 09:29:28 +0800

    Support multiple upload

--- src/app/Upload.php
+++ src/app/Upload.php
@@ -11,8 +11,12 @@ class Upload extends \Web
 
     function post($f3)
     {
+        $data = [];
         $f3->set('UPLOADS', WEB . '/images/');
-        list($receive) = array_keys(parent::receive(null, true, false));
-        echo $f3->get('BASE') . '/images/' . preg_replace('/^.+[\\\\\\/]/', '', $receive);
+        $receive = array_keys(parent::receive(null, true, false));
+        foreach ($receive as $item) {
+            $data[] = $f3->get('BASE') . '/images/' . preg_replace('/^.+[\\\\\\/]/', '', $item);
+        }
+        echo json_encode(['images' => $data], JSON_UNESCAPED_UNICODE);
     }
 }
