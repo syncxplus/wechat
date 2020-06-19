1ef772e97c3833bcc23ea70937acaff6458b0c4c
jibo <jibo@outlook.com>
2020-06-19 11:42:56 +0800

    Verify before upload

--- src/app/Upload.php
+++ src/app/Upload.php
@@ -4,8 +4,11 @@ namespace app;
 
 class Upload extends \Web
 {
-    function get()
+    private $code;
+
+    function get(\Base $f3)
     {
+        $f3->set('code', $this->code);
         echo \Template::instance()->render('upload.html');
     }
 
@@ -17,5 +20,12 @@ class Upload extends \Web
             $data[] = $f3->get('BASE') . '/data/uploads/' . preg_replace('/^.+[\\\\\\/]/', '', $item);
         }
         echo json_encode(['images' => $data], JSON_UNESCAPED_UNICODE);
+    }
+
+    function beforeRoute(\Base $f3) {
+        $this->code = $f3->get('REQUEST.code') ?? false;
+        if (!$this->code || $this->code != $f3->get('DEFAULT_VERIFY_CODE')) {
+            $f3->error('Unauthorized access');
+        }
     }
 }
